49ccfb230cff056c7a6cb97f86243528400cda2d
Arthur Melo <arthurhsmelo@gmail.com>
2019-08-21 00:17:36 -0300

    firebase

--- service.js
+++ service.js
@@ -1,16 +1,16 @@
-// var firebase = require("firebase/app");
-// var firebaseConfig = {
-//   apiKey: "AIzaSyA2fFvZfjVLweMg_NZnnGcS03L_oSi04r0",
-//   authDomain: "strider-bug-hunters.firebaseapp.com",
-//   databaseURL: "https://strider-bug-hunters.firebaseio.com",
-//   projectId: "strider-bug-hunters",
-//   storageBucket: "strider-bug-hunters.appspot.com",
-//   messagingSenderId: "114071764524",
-//   appId: "1:114071764524:web:25cc39625ad275b1"
-// };
-// firebase.initializeApp(firebaseConfig);
-// require("firebase/firestore");
+var firebase = require("firebase/app");
+var firebaseConfig = {
+  apiKey: "AIzaSyA2fFvZfjVLweMg_NZnnGcS03L_oSi04r0",
+  authDomain: "strider-bug-hunters.firebaseapp.com",
+  databaseURL: "https://strider-bug-hunters.firebaseio.com",
+  projectId: "strider-bug-hunters",
+  storageBucket: "strider-bug-hunters.appspot.com",
+  messagingSenderId: "114071764524",
+  appId: "1:114071764524:web:25cc39625ad275b1"
+};
+firebase.initializeApp(firebaseConfig);
+require("firebase/firestore");
 
-// const db = firebase.firestore(firebase.app());
+const db = firebase.firestore(firebase.app());
 
-// module.exports = db;
+module.exports = db;
