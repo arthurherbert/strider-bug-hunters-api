8ed48501d8a9735748fed118feb476d166c59344
Arthur Melo <arthurhsmelo@gmail.com>
2019-08-20 23:22:01 -0300

    test

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var serverless = require("serverless-http");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var teamsRouter = require("./routes/teams");
 var eventsRouter = require("./routes/events");
 
 var app = express();
-
+app.set("view engine", "html");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
